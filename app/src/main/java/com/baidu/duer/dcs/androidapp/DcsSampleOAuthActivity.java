094bd68747ccf9a310bc5b02746cc02677081ff1
mahuaizhi <mahuaizhi@ling.ai>
2017-07-12 16:20:56 +0800

    init project

--- app/src/main/java/com/baidu/duer/dcs/androidapp/DcsSampleOAuthActivity.java
+++ app/src/main/java/com/baidu/duer/dcs/androidapp/DcsSampleOAuthActivity.java
@@ -38,7 +38,7 @@ import com.baidu.duer.dcs.util.LogUtil;
 public class DcsSampleOAuthActivity extends DcsSampleBaseActivity implements View.OnClickListener {
     // 需要开发者自己申请client_id
     // client_id，就是oauth的client_id
-    private static final String CLIENT_ID = "{{placeholder for your client_id}}";
+    private static final String CLIENT_ID = "1udLzmTG2KGmKGmPkwZZe1gm";   // f2lgi8zblEYNzdvidDAS3lkrMP4xqmNs     07mqKwifQui64u3vZRFyGsdcAdLFcpMz
     // 是否每次授权都强制登陆
     private boolean isForceLogin = false;
     // 是否每次都确认登陆
